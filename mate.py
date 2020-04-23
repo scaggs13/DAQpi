09722e7562f2f73000976443d0a288d3c195b2b4
scaggs13 <samuelcscaggs@student.cofo.edu>
2020-04-23 13:05:38 -0500

    Trying to get Solar mate 3 data to work

--- mate.py
+++ mate.py
@@ -17,7 +17,7 @@ def addmate3data(jsonFile):
     jsonFile['Inverter'] = {}
     try:
         r = requests.get(url=httpCall, params=PARAMS)
-        data = json.dumps(r.content)
+        data = json.loads(r.content)
         # Fetch all of these from the Mate 3s
         # Change the port number to match the corresponding object.
         print(data)
