e7169927617863f8f3f94bb450cd678977618bfd
scaggs13 <samuelcscaggs@student.cofo.edu>
2020-04-23 13:45:52 -0500

    Trying to get Solar mate 3 data to work

--- mate.py
+++ mate.py
@@ -6,9 +6,10 @@ mateIP = '173.218.91.129:3000'
 httpCall = 'http://' + mateIP + '/Dev_status.cgi'
 PARAMS = {'Port': 0}
 
-battery = ["Shunt_A_I", "Shunt_A_AH", "Shunt_A_kWh", "Shunt_B_I", "Shunt_B_AH", "Shunt_B_kWh", "SOC", "Min_SOC", "Days_since_full","CHG_parms_met", "In_AH_today", "Out_AH_today", "In_kWh_today", "Out_kWh_today", "Net_CFC_AH", "Net_CFC_kWh", "Batt_V", "Batt_temp"]
+battery = ["Shunt_A_I", "Shunt_A_AH", "Shunt_A_kWh", "SOC", "Min_SOC", "Days_since_full","CHG_parms_met", "In_AH_today", "Out_AH_today", "In_kWh_today", "Out_kWh_today", "Net_CFC_AH", "Net_CFC_kWh", "Batt_V", "Batt_temp"]
 chargeController = ["Out_I", "In_I", "Batt_V", "In_V", "Out_kWh", "Out_AH"]
-inverter = ["Inv_I", "Chg_I", "Buy_I", "Sell_I", "VAC_in", "VAC_out", "Batt_V"]
+inverter = ["Inv_I_L2","Chg_I_L2","Buy_I_L2","Sell_I_L2","VAC1_in_L2","VAC2_in_L2","VAC_out_L2","Batt_V"]
+
 
 def addmate3data(jsonFile):
     jsonFile['Battery'] = {}
@@ -40,7 +41,6 @@ def addmate3data(jsonFile):
                     else:
                         tmpBatt[battery[x]] = 0
                 jsonFile['Battery'][battery[x]] = tmpBatt[battery[x]]
-                print(jsonFile)
             print(jsonFile)
         for x in range(0, len(chargeController)):
             if tmpMCC:
@@ -53,7 +53,7 @@ def addmate3data(jsonFile):
         print(jsonFile)
     except requests.exceptions.RequestException as e:
         print e
-        jsonFile['Battery'] = {"Shunt_A_I": 0, "Shunt_A_AH": 0, "Shunt_A_kWh": 0.0, "Shunt_B_I": 0.0, "Shunt_B_AH": 0, "Shunt_B_kWh": 0.000, "SOC": 0, "Min_SOC": 0, "Days_since_full": 0.0,"CHG_parms_met": False, "In_AH_today": 0, "Out_AH_today": 0, "In_kWh_today": 0.0, "Out_kWh_today": 0.0, "Net_CFC_AH": 0, "Net_CFC_kWh": 0.0, "Batt_V": 0.0, "Batt_temp": 0.0}
+        jsonFile['Battery'] = {"Shunt_A_I": 0, "Shunt_A_AH": 0, "Shunt_A_kWh": 0.0, "SOC": 0, "Min_SOC": 0, "Days_since_full": 0.0,"CHG_parms_met": False, "In_AH_today": 0, "Out_AH_today": 0, "In_kWh_today": 0.0, "Out_kWh_today": 0.0, "Net_CFC_AH": 0, "Net_CFC_kWh": 0.0, "Batt_V": 0.0, "Batt_temp": 0.0}
         for x in range(len(chargeController)):
             jsonFile['ChargeControllerP'][chargeController[x]] = 0.0
             jsonFile['ChargeControllerM'][chargeController[x]] = 0.0
